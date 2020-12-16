a29a6e8d8ebe7c5c16ef8a901d380b471749f2b0
Christian Krivickis <S00198911@mail.itsligo.ie>
2020-12-16 20:15:28 +0000

    Add project files.

---
+++ CA2/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void CalculateMonthlyPay();
+
+
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{FirstName} {LastName}");
+        }
+    }
+
+
+    public class FullTimeEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+
+        public override void CalculateMonthlyPay()
+        {
+            Salary = Salary / 12;
+        }
+
+    }
+
+
+    public class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+
+        public override void CalculateMonthlyPay()
+        {
+            //HourlyRate * HoursWorked;
+        }
+    }
+}
