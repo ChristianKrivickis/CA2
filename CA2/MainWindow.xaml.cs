5dbcb3ac9cca9659f61ca5e8d93b52d1f90fb2f5
Christian Krivickis <S00198911@mail.itsligo.ie>
2020-12-16 21:35:11 +0000

    Edited xaml

--- CA2/MainWindow.xaml.cs
+++ CA2/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,14 +16,26 @@ using System.Windows.Shapes;
 
 namespace CA2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>(); 
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PartTimeEmployee pt1 = new PartTimeEmployee("Jane", "Jones", "Part Time", 10, 15);
+            PartTimeEmployee pt2 = new PartTimeEmployee("John", "Smith", "Part Time", 10, 15);
+
+
+            employees.Add(pt1);
+            employees.Add(pt2);
+
+            listBox.ItemsSource = employees;
         }
     }
 }
